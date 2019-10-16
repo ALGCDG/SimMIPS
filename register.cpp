ba4eb5e5cd8079138e0efb571e334a013ff36a97
Archibald Crichton <ac11018@ic.ac.uk>
2019-10-16 21:35:44 +0100

    My first attempt at implementing the registers as a object

---
+++ register.cpp
@@ -0,0 +1,68 @@
+#include"register.hpp"
+
+unsigned int simulated_register::general_purpose_registors[] = {
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+};
+
+//getters
+unsigned int simulated_register::read_register(const unsigned int& register_number) const
+{
+    return general_purpose_registors[register_number];
+}
+unsigned int simulated_register::read_HI() const
+{
+    return HI;
+}
+unsigned int simulated_register::read_LO() const
+{
+    return LO;
+}
+
+//setters
+void simulated_register::write_register(const unsigned int& register_number, const unsigned int& value)
+{
+    // ensure that register $0 is immutable
+    if ( register_number > 0 && register_number < 32 )
+    {
+        general_purpose_registors[register_number] = value;
+    }
+}
+void simulated_register::write_HI(const unsigned int& value)
+{
+    HI = value;
+}
+void simulated_register::write_LO(const unsigned int &value)
+{
+    LO = value;
+}
---
+++ register.hpp
@@ -0,0 +1,20 @@
+// a header file defining the register class
+
+class simulated_register
+{
+    // unsure of how to implement program counter
+    private:
+    static unsigned int general_purpose_registors[32];
+    static unsigned int HI, LO;
+
+    public:
+    //getters
+    static unsigned int read_register(const unsigned int& register_number);
+    static unsigned int read_HI();
+    static unsigned int read_LO();
+
+    //setters
+    static void write_register(const unsigned int& register_number, const unsigned int& value);
+    static void write_HI(const unsigned int& value);
+    static void write_LO(const unsigned int &value);
+};
