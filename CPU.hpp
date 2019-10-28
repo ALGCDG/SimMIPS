f389af9a67924e23bd8f59b9dc396c1bb42157c2
Archibald Crichton <ac11018@skee-305-015.ee.ic.ac.uk>
2019-10-28 18:16:47 +0000

    removing function pointer definitions from CPU.hpp, they should only appear once in R/I/J.hpp

--- CPU.hpp
+++ CPU.hpp
@@ -10,13 +10,6 @@
 typedef unsigned int uint;
 typedef unsigned char uchar;
 
-typedef void (*R_OPCODE)(const uchar &, const uchar &, const uchar &, const uchar &);
-// input arguments are rs(5), rt(5), rd(5), shamt(5)
-typedef void (*I_OPCODE)(const uchar &, const uchar &, const uint &);
-// input arguments are rs(5), rt(5), immediate(16)
-typedef void (*J_OPCODE)(const uint &);
-// input arguments are address(26)
-
 class CPU
 {
     private:
