bc8beca2eac6b6fa913464aa846987d9940fd0a6
jrpdotjpg <jrp2018@ic.ac.uk>
2019-10-29 14:18:30 +0000

    Updated hpp, comments for used / unused funcs

--- fileManager.hpp
+++ fileManager.hpp
@@ -8,9 +8,22 @@ private:
 public:
 	//TODO update functions
 	fileManager(std::string path); //constr
-	void jump_to_offset(int offset); //sets ifstream to point to offset instruction
-	//void increment_offset(); //function for clarity, increments offset 4 bytes
-	void goto_next_instr();
-	//void set_offset(int offset_in); //sets offset to any number
-	std::bitset<32> fetch_instr_next_instr(); //fetches instruction from whatever the filestream is pointing at
+	//confusing use of offset:
+	//offset in this sense is the logical location (not offset eg base + offset)
+	uint jump_r_w_left_return(int offset); //used
+	uint jump_r_w_right_return(int offset);//used
+	void jump_to_offset(int offset);//used
+	int get_currOffset();//used
+	uint jump_r_word_return(int offset);//used
+	uint r_word_maintain(); //potentially unused
+	uint jump_r_word_advance(int offset);//potentially unused
+	uint r_word_advance();//used
+	uint jump_r_byte_return(int offset);//used
+	uint r_byte_advance(); //potentially unused
 }
+//potentially unused commment means:
+//Function created for initial design architecture.
+//In current design: not used, extraneous function,
+//eg jump read word return uses jump, r_word_advance, then return
+// no need for eg r_word_maintain 
+//
