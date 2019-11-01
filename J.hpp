3298be9886298fdd3c6ff5ca7e10abb84ddc5ddb
Archie <archie@dyn3208-161.wlan.ic.ac.uk>
2019-11-01 13:42:47 +0000

    realised J type instructions also require to reference

--- J.hpp
+++ J.hpp
@@ -1,15 +1,16 @@
 #include "simulated_memory.hpp"
+#include "register.hpp"
 
 typedef unsigned int uint;
 typedef unsigned char uchar;
 
-typedef void (*J_OPCODE)(const uint &, simulated_memory&);
+typedef void (*J_OPCODE)(const uint &, simulated_register&, simulated_memory &);
 // input arguments are address(26) AND a reference to the memory block
 
 class J_TYPE // also inherits from memory
 {
     public:
     // J type operators declarations
-    void J(const uint &address, simulated_memory &mem);
-    void JAL(const uint &address, simulated_memory &mem);
+    void J(const uint &address, simulated_register &reg, simulated_memory &mem);
+    void JAL(const uint &address, simulated_register &reg, simulated_memory &mem);
 };
