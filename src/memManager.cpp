185406600bf59b1bd79e618b34901f31bc1a70a0
jrpdotjpg <jrp2018@ic.ac.uk>
2019-11-19 18:38:45 +0000

    Lots of fixes to weird behaviours eugh, sync w/ Archie

--- src/memManager.cpp
+++ src/memManager.cpp
@@ -1,5 +1,5 @@
 #include "memManager.hpp"
-
+#include <iostream>
 
 //need constructor
 memManager::memManager() : DATA(0x4000000, 0)
@@ -12,7 +12,10 @@ uint memManager::read_word(int index){
 }
 
 void memManager::store_word(int index, uint word){
+    std::cerr << "Word stored: " << std::hex << word << std::endl;
     for(int i = 0; i < 4; i++){
-        DATA[3-i] = (word >> i*8) & 0xFF;
+        DATA[3-i+index] = (word >> i*8) & 0xFF;
     }
-}
+    //std::cerr << "Word at word alligned address written to: ";
+    //std::cerr << std::hex << DATA[index] + (DATA[index+1] << 8) + (DATA[index+2] << 16) + (DATA[index+3] << 24) << std::endl;
+ }
