1afab9ebc3c3a719ae5bbb96f0f12f3fd4d9c49f
Jonathan Paynter <jrp2018@skee-304-023.ee.ic.ac.uk>
2019-11-21 20:14:35 +0000

    I can't remember what has changed but it should be a net positive

--- src/ioManager.cpp
+++ src/ioManager.cpp
@@ -4,12 +4,20 @@ uint ioManager::read_word()
 {
     int input;
     input = std::getchar();
+    if(ferror(stdin)){
+      std::cerr<< "Error with stdin" << std::endl;
+      exit(-21);
+    }
     return static_cast<uint>(input);
 }
 
 void ioManager::store_word(uint word){
     char out = 0xFF & word;
     std::putchar(out);
+    if(ferror(stdin)){
+      std::cerr<< "Error with stdin" << std::endl;
+      exit(-21);
+    }
     return;
 }
 
