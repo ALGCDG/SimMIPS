a6f75f4f537106ed9410f9bb1785c7fdf7140717
jrpdotjpg <jrp2018@ic.ac.uk>
2019-11-21 12:51:44 +0000

    New testbenches and fixed LH behaviour

---
+++ mips_testbench/tests/LH/0/assembly.s
@@ -0,0 +1,40 @@
+#register needs 2^7 2^5 2^3 2^1: 0b10000000 00100000 00001000 00000010
+#memory needs   2^6 2^4 2^2 2^0  0b01000000 00010000 00000100 00000001
+#(interleving bits for unique sum)
+
+#destination register REG2
+li $2, 0b10000000001000000000100000000010
+
+#source register (storing) REG11
+li $11, 0b01000000000100000000010000000001
+
+#memory base
+li $1, 0x20000000
+
+sw $11, 0($1) #CHECKING: CORRECT WORD IS STORED
+
+#REMEMBER, LH should sign extend
+#===============
+
+
+lh $2, 0($1)
+#should load 0b00000000 00000000 01000000 00010000
+#so no sign extension
+#64+16=80
+
+#==============
+
+#MASK IN REG7
+li $7, 0xFF
+
+add $3, $2, $0
+and $3, $3, $7
+#3 now contains just bottom byte
+
+srl $2, $2, 8
+#2 now contains second to bottom byte
+
+add $2, $2, $3
+#2 now contains sum of bytes of half word loaded in
+
+jr $0
